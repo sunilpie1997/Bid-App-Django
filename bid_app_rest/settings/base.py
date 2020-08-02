e48999be6c3d293cf844429272dbc85c7e33ee5a
sunilpie <sunilpie1997@gmail.com>
2020-08-02 15:55:36 +0530

    testing with allowing cross origin requests

--- bid_app_rest/settings/base.py
+++ bid_app_rest/settings/base.py
@@ -31,12 +31,12 @@ AUTH_USER_MODEL="accounts.BidAppUser"
 
 # Application definition
 
-#CORS_ORIGIN_ALLOW_ALL=True 
-
+CORS_ORIGIN_ALLOW_ALL=True 
+"""
 CORS_ORIGIN_WHITELIST = [
     "https://bid-app-project.herokuapp.com",
 ]
-
+"""
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
