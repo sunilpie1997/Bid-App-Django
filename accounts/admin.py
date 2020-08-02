8d1b5c3b5d8ec9e4906804b00927e61c03dd300b
sunilpie <sunilpie1997@gmail.com>
2020-08-02 15:38:53 +0530

    adding 'ProfileImageModel' to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile,BidAppUser
+from .models import Profile,BidAppUser,ProfileImageModel
 from django.contrib.auth.admin import UserAdmin
+
 
 admin.site.register(BidAppUser, UserAdmin)
 admin.site.register(Profile)
+admin.site.register(ProfileImageModel)
